bb00e376d549de86ad9f79d93a19a15d469865d1
beistvan <iberegsz@student.42vienna.com>
2024-10-04 01:58:27 +0200

    Update cpp00-ex00-output-checker.sh

--- cpp00-ex00-output-checker.sh
+++ cpp00-ex00-output-checker.sh
@@ -6,7 +6,7 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-echo -e "Comparing two logs: one from running the megaphone and one from the subject.\n"
+echo "Comparing two logs: one from running the megaphone and one from the subject."
 
 executable_name="megaphone"
 
@@ -22,20 +22,29 @@ expected_outputs=(
     "* LOUD AND UNBEARABLE FEEDBACK NOISE *"
 )
 
+passed_tests=0
+
 for i in "${!test_cases[@]}"; do
     test_case=${test_cases[$i]}
     expected_output=${expected_outputs[$i]}
 
-    echo "Test case $((i + 1)): ./${executable_name} $test_case"
+    echo -e "\nTest case $((i + 1)): ./${executable_name} $test_case"
 
     megaphone_log=$(eval ./${executable_name} "$test_case")
 
     echo "Megaphone: $megaphone_log"
-    echo -e "Expected : $expected_output\n"
+    echo "Expected : $expected_output"
 
     if [ "$megaphone_log" = "$expected_output" ]; then
         echo "Test case $((i + 1)) passed.✅"
+        ((passed_tests++))
     else
         echo "Test case $((i + 1)) failed.❌"
     fi
 done
+
+if [ $passed_tests -eq ${#test_cases[@]} ]; then
+    echo -e "\n\033[0;32mAll tests passed 😊 ($passed_tests of ${#test_cases[@]}\033[0m)"
+else
+    echo -e "\n\033[0;31mPassed $passed_tests of ${#test_cases[@]}\033[0m"
+fi
