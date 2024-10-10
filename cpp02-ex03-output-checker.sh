2f71a3c8b82aa360cdd3c353f0bc06e6ab751bff
beistvan <beistvan@gmail.com>
2024-10-10 23:27:51 +0200

    cpp02-ex03-output-checker.sh

---
+++ cpp02-ex03-output-checker.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+EXECUTABLE_NAME="bsp"
+
+EXPECTED_OUTPUT="$(cat <<EOF
+A: (-2.17969, 1.62891)
+B: (3.60156, 2.48828)
+C: (-1.17969, -2.03125)
+D: Inside: (1.30078, 1.64844)
+E: Outside: (-2.19922, -1.60938)
+inside: yes
+outside: yes
+EOF
+)"
+
+make > /dev/null
+
+if [ ! -f $EXECUTABLE_NAME ]; then
+    echo "Executable ($EXECUTABLE_NAME) not found"
+    exit 1
+fi
+
+diff <(./$EXECUTABLE_NAME) <(echo "$EXPECTED_OUTPUT")
+
+if [ $? -eq 0 ]; then
+    echo "Output is correct"
+else
+    echo "Output is incorrect"
+fi
